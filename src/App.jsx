509093727ccc9f63b24dd6d844909b0bed2e2f31
Asief Mahir <asiefmahir1@gmail.com>
2026-02-26 00:23:10 +0600

    feat: Learn about state mechanism in React

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,69 @@
+import { useState } from "react";
 import "./App.css";
-import BioData from "./components/BioData";
-
-const bioDataInfos = [
-	{
-		name: "Mahir Asief",
-		phone: "+4245435435",
-		email: "asiefmahir1@gmail.com",
-		skills: ["Js", "React"],
-		interests: ["Football", "Chess", "System Design"],
-		address: "Tejgaon, Dhaka",
-	},
-	{
-		name: "Shamim Vai",
-		phone: "+245412.2.",
-		email: "shamim@gmail.com",
-		skills: ["Js", "React", "CSS", "WP"],
-		interests: ["Football", "CRICKET"],
-	},
-	{
-		name: "Riad Vai",
-		phone: "+245412.2.",
-		email: "shamim@gmail.com",
-		skills: ["Js", "React", "CSS", "WP"],
-		interests: ["Football", "CRICKET"],
-	},
-];
 
 function App() {
+	// states
+	const [todoTitle, setTodoTitle] = useState("");
+	const [todoList, setTodoList] = useState([
+		// { id: "1", title: "Demo Todo 1" },
+		// { id: "2", title: "Demo Todo 2" },
+	]);
+
+	// delete -> non-mutative // no -ai code
+
+	// edit/update
+
+	// state, lifting, context api
+
+	// event -> state
+
+	const submitHandler = (event) => {
+		event.preventDefault();
+
+		if (!todoTitle.trim()) {
+			return alert(`Please enter a valid todo Title`);
+		}
+
+		const newTodo = {
+			id: Date.now() + "",
+			title: todoTitle,
+		};
+
+		// todoList.push(newTodo);
+		setTodoList([newTodo, ...todoList]); // non-mutative way
+		setTodoTitle("");
+	};
 	return (
 		<div className="app">
-			<h2>BioData Demo App</h2>
-			{bioDataInfos.map((bioData) => (
-				<div>
-					<BioData
-						key={bioData.name}
-						name={bioData.name}
-						phone={bioData.phone}
-						email={bioData.email}
-						skills={bioData.skills}
-						interests={bioData.interests}
-					/>
-					<hr />
-				</div>
-			))}
+			<h2>Todo App</h2>
+			<form onSubmit={submitHandler}>
+				<input
+					type="text"
+					value={todoTitle}
+					onChange={(event) => setTodoTitle(event.target.value)}
+				/>
+				<button type="submit">Create Todo</button>
+			</form>
+			<ul>
+				{todoList.map((todo) => (
+					<li key={todo.id}>
+						<span>{todo.title}</span>
+						<button>Edit</button>
+						<button>Remove</button>
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 }
+
+// function sum(a, b) {
+// 	return a + b;
+// }
+
+// sum(10, 20); //
+
+// document.getElementById("btn").addEventListener("click", () => {})
 
 // BioData()
 
